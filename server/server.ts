86db6cd61374432179e0f90b990f06bfe1451c39
Atte <attehiltunen@hotmail.com>
2025-02-06 12:44:56 +0200

    Server side testing in production

--- server/server.ts
+++ server/server.ts
@@ -6,7 +6,7 @@ const PORT = 3000;
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript!');
+    res.send({msg:'Hello, TypeScript!'}).json();
 });
 
 app.listen(PORT, () => {
