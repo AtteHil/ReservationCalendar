0a9db65cb4b49217ced573e4f0802af88314c489
Atte <attehiltunen@hotmail.com>
2025-02-06 12:39:37 +0200

    fixes to server scripts to run in production

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
-
+import cors from 'cors';
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript!');
