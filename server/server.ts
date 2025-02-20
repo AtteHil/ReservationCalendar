9ccbff57aa38eb9e42d11cef7a2bfdbd1cd24cf1
Atte <attehiltunen@hotmail.com>
2025-02-21 00:00:22 +0200

    server mongo connection + routes folder

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,23 @@
-import express, { Request, Response } from 'express';
+import express, { Express }  from 'express';
+import mongoose, { Connection } from 'mongoose';
+import morgan from 'morgan';
 import cors from 'cors';
-const app = express();
-const PORT = 3000;
+import router from './routes/mongo';
 
+const app: Express = express();
+const port: number = parseInt(process.env.PORT as string) || 3000;
+const mongoUri: string = process.env.MONGO_URI as string;
+mongoose.connect(mongoUri);
+mongoose.Promise = Promise;
+const db: Connection = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use('/', router);
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({msg:'Hello, TypeScript!'});
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
 });
