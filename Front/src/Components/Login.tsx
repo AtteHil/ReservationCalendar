b594bae7f6a5cf22472e0c6a5518d37b1e797456
AtteHil <attehiltunen@hotmail.com>
2025-02-21 23:00:41 +0200

    remove never read value

--- Front/src/Components/Login.tsx
+++ Front/src/Components/Login.tsx
@@ -9,7 +9,7 @@ import { useTranslation } from 'react-i18next';
 
 
 export default function CredentialsSignInPage() {
-    const { t, i18n } = useTranslation()
+    const { t } = useTranslation()
     return (
 
         <Box id='loginForm'>
